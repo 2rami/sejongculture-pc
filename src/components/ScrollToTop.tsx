02391d8e6842bda2a9f69547082a52b7ce571475
2rami <86802443+2rami@users.noreply.github.com>
2025-06-22 17:57:47 +0900

    스크롤 왜자꾸ㅡㅡ

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -5,7 +5,18 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // 즉시 스크롤
     window.scrollTo(0, 0);
+    
+    // 추가적으로 브라우저 렌더링이 완료된 후에도 스크롤
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+    
+    // 더 강력한 백업으로 약간의 지연 후에도 스크롤
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
   }, [pathname]);
 
   return null;
